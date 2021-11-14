33c3de9a2bace5e0f672a57961fec1b9518c222e
Daniel Danielecki <daniel.danielecki@mail.com>
2021-11-14 02:47:31 +0100

    Add: placeholder for pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -14,42 +15,35 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to DEX of{" "}
+          <a
+            href="https://ton.org"
+            rel="noopener noreferrer"
+            target="_blank"
+            title="The Open Network"
+          >
+            TON
+          </a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        <p className={styles.description}>Placeholder for Home description.</p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <Link href="/create-pool">
+            <a className={styles.card}>Create Pool</a>
+          </Link>
+          <Link href="/exchange-token">
+            <a className={styles.card}>Exchange Token</a>
+          </Link>
+          <Link href="/liquidity-from-pool">
+            <a className={styles.card}>Liquidity from Pool</a>
+          </Link>
+          <Link href="/list-of-pools">
+            <a className={styles.card}>List of Pools</a>
+          </Link>
+          <Link href="/pool-statistics">
+            <a className={styles.card}>Pool Statistics</a>
+          </Link>
         </div>
       </main>
 
