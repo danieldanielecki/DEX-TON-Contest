b3978ed9441daeef3ec38fa4fc4ee4d1211199e3
Daniel Danielecki <daniel.danielecki@mail.com>
2021-11-16 09:31:47 +0100

    Fix: broken .css imports

--- pages/create-pool.tsx
+++ pages/create-pool.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 const CreatePool: NextPage = () => {
   return (
