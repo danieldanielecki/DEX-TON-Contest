41d49e45f335ef8dc007d232832100c0bc053783
Daniel Danielecki <daniel.danielecki@mail.com>
2021-11-18 11:26:51 +0100

    Refactor: duplicated select currency code in 2 components to 1 component

--- components/SelectCurrency.tsx
+++ components/SelectCurrency.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const SelectCurrency = (props: { currencies: any; onSelectCurrency: any }) => {
+const SelectCurrency = (props: {
+  currencies: any;
+  isOne: boolean;
+  onSelectCurrency: any;
+}) => {
+  const { currencies, isOne, onSelectCurrency } = props;
+  let filteredCurrencies;
+
+  if (isOne) {
+    filteredCurrencies = currencies.filter(
+      (currency: { code: string }) => currency.code !== "TUSD"
+    );
+  } else {
+    filteredCurrencies = currencies;
+  }
+
   return (
-    <select onChange={(e) => props.onSelectCurrency(e.target.value)}>
-      {props.currencies.map((currency: { code: string; name: string }) => {
+    <select onChange={(e) => onSelectCurrency(e.target.value)}>
+      {filteredCurrencies.map((currency: { code: string; name: string }) => {
         const { code, name } = currency;
         return (
           <option key={code} value={code}>
