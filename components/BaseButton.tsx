ec73ef23b6ccdd990058fd76dc5095efc973eaf1
Daniel Danielecki <daniel.danielecki@mail.com>
2021-11-19 13:33:01 +0100

    Add: props in BaseButton for additional style classes

--- components/BaseButton.tsx
+++ components/BaseButton.tsx
@@ -1,18 +1,24 @@
 import styles from "../styles/Buttons.module.scss";
 import PropTypes from "prop-types";
 
-const BaseButton = (props: { title: string }) => {
-  const { title } = props;
+const BaseButton = (props: {
+  additionalStyleClasses?: string;
+  title: string;
+}) => {
+  const { title, additionalStyleClasses } = props;
 
   return (
     <div className={styles.button_container}>
       <span className={styles.mas}>{title}</span>
-      <button type="button">{title}</button>
+      <button className={`${additionalStyleClasses}`} type="button">
+        {title}
+      </button>
     </div>
   );
 };
 
 BaseButton.propTypes = {
+  additionalStyleClasses: PropTypes.string,
   title: PropTypes.string.isRequired,
 };
 
