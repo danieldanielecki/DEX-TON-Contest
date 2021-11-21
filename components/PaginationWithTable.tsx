e361cc8e99d97c518523315fb3d021e12b1d1e05
Daniel Danielecki <daniel.danielecki@mail.com>
2021-11-21 16:23:55 +0100

    Change: outsource table data to separated file

--- components/PaginationWithTable.tsx
+++ components/PaginationWithTable.tsx
@@ -1,6 +1,7 @@
 // src/components/pagination.table.js
 import "regenerator-runtime/runtime";
 import React from "react";
+import { TABLE_DATA } from "../config/data/pools/dummy-data";
 
 import {
   useTable,
@@ -247,139 +248,7 @@ function PaginationTableComponent() {
     []
   );
 
-  const data = [
-    {
-      firstName: "committee-c15dw",
-      lastName: "editor-ktsjo",
-      age: 3,
-      visits: 46,
-      progress: 75,
-      status: "relationship",
-    },
-    {
-      firstName: "midnight-wad0y",
-      lastName: "data-7h4xf",
-      age: 1,
-      visits: 56,
-      progress: 15,
-      status: "complicated",
-    },
-    {
-      firstName: "tree-sbdb0",
-      lastName: "friendship-w8535",
-      age: 1,
-      visits: 45,
-      progress: 66,
-      status: "single",
-    },
-    {
-      firstName: "chin-borr8",
-      lastName: "shirt-zox8m",
-      age: 0,
-      visits: 25,
-      progress: 67,
-      status: "complicated",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-    {
-      firstName: "women-83ef0",
-      lastName: "chalk-e8xbk",
-      age: 9,
-      visits: 28,
-      progress: 23,
-      status: "relationship",
-    },
-  ];
-  console.log(JSON.stringify(data));
-
-  return <Table columns={columns} data={data} />;
+  return <Table columns={columns} data={TABLE_DATA} />;
 }
 
 export default PaginationTableComponent;
