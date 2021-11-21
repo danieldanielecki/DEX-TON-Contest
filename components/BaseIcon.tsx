8c53b1cf2e109e464715454d3a9d3361340a7136
Katarzyna Janiszewska <katarzyna.janiszewska@gmail.com>
2021-11-21 11:00:16 +0100

    resolved conflicts

--- components/BaseIcon.tsx
+++ components/BaseIcon.tsx
@@ -13,7 +13,6 @@ const loadModule = () => {
   }
 }
   return (
-<<<<<<< HEAD
     <div>
       <img
         height={64}
@@ -21,16 +20,6 @@ const loadModule = () => {
         width={64}
       />
     </div>
-=======
-    <img
-      height={64}
-      src={
-        require(`../node_modules/cryptocurrency-icons/svg/color/${name}.svg`)
-          .default.src
-      }
-      width={64}
-    />
->>>>>>> 1fc39fd54af2a8fb8844fd2babfbba30e5e53b93
   );
 };
 
--- stubs/generateStubs/generateFiles.js
+++
@@ -1,6 +0,0 @@
-import currencies from '../currencies/list-currencies.json';
-
-function generateSympols() {
-  const symbols = currencies.reduce((prev, cur) => prev.concat(' ', cur));
-  console.log(symbols);
-}
