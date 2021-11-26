8605d88b3f26fda630389ab28b622cd77d8e4b3d
Katarzyna Janiszewska <katarzyna.janiszewska@gmail.com>
2021-11-26 21:11:08 +0100

    cleaned files

--- components/BuySellSummary.tsx
+++ components/BuySellSummary.tsx
@@ -35,55 +35,46 @@ const BuySellSummary = (props: {
     }
   });
 
-
   return (
-    <>
-      {/* <BaseButton title="calculate" onClick={handleCalculation} />
-      {isCalcVisible ? */}
-      <div>
-        <div className="w-500 d-flex">
-          {!!currencyA
-            ? (
-              <div>
-                <BaseIcon image={currencyA.image} />
-                <p className="d-flex flex-column">
-                  <span>
-                    {currencyA.name}
-                  </span>
-                  <span>
-                    {currencyA.current_price} USD
-                  </span>
-                </p>
-              </div>
-            ) : ''}
-          {amount
-            && !!method
-            && !!currencyA
-            && !!currencyB
-            ? <span>{amountCalc} USD</span>
-            : ''}
-          <BaseIcon image="swap-icon.svg" />
-          {!!currencyB
-            ? (
-              <div>
-                <BaseIcon image={currencyB.image} />
-                <p className="d-flex flex-column">
-                  <span>
-                    {currencyB.name}
-                  </span>
-                  <span>
-                    {currencyB.current_price} USD
-                  </span>
-                </p>
-              </div>)
-            : ''}
-        </div>
-        <div>
-          {/* {method.type} {amount} {currencyA.name} with {amountCalc} {currencyB.name} */}
-        </div>
+    <div>
+      <div className="w-500 d-flex">
+        {!!currencyA
+          ? (
+            <div>
+              <BaseIcon image={currencyA.image} />
+              <p className="d-flex flex-column">
+                <span>
+                  {currencyA.name}
+                </span>
+                <span>
+                  {currencyA.current_price} USD
+                </span>
+              </p>
+            </div>
+          ) : ''}
+        {amount
+          && !!method
+          && !!currencyA
+          && !!currencyB
+          ? <span>{amountCalc} USD</span>
+          : ''}
+        <BaseIcon image="swap-icon.svg" />
+        {!!currencyB
+          ? (
+            <div>
+              <BaseIcon image={currencyB.image} />
+              <p className="d-flex flex-column">
+                <span>
+                  {currencyB.name}
+                </span>
+                <span>
+                  {currencyB.current_price} USD
+                </span>
+              </p>
+            </div>)
+          : ''}
       </div>
-      {/* : ''} */}
-    </>
+    </div>
   );
 };
 
