9abb3f608da2692312deb430ea66f96c3187c1e1
Katarzyna Janiszewska <katarzyna.janiszewska@gmail.com>
2021-11-27 16:12:37 +0100

    removed redundant logic

--- pages/liquidity-from-pool.tsx
+++ pages/liquidity-from-pool.tsx
@@ -6,10 +6,8 @@ import useToggleAlert from "../hooks/useToggleAlert";
 import AmountInput from "../components/AmountInput";
 import BaseButton from "../components/BaseButton";
 import BaseDialog from "../components/BaseDialog";
-import BaseIcon from "../components/BaseIcon";
 import SelectCurrency from "../components/SelectCurrency";
-import { useState, ChangeEvent, Fragment } from "react";
-import { CURRENCIES } from "../config/data/currency-exchanges/dummy-exchanges";
+import { Fragment } from "react";
 import store from '../redux/store';
 import { clearSelected } from '../redux/actions/selectedActions';
 
@@ -19,35 +17,7 @@ const LiquidityFromPool: NextPage = (props: {
   useEffect(() => {
     store.dispatch(clearSelected());
   }, [])
-  const tether = CURRENCIES.currencies[5];
-  const [currencies, setCurrencies] = useState(CURRENCIES.currencies);
-  const [currencyA, setCurrencyA] = useState(CURRENCIES.currencies[0]);
-  const [currencyB, setCurrencyB] = useState(tether);
-  const [currencyAval, setCurrencyAval] = useState(
-    CURRENCIES.currencies[0].sellRate
-  );
-  const [currencyBval, setCurrencyBval] = useState(tether.sellRate);
   const [isOpened, setIsOpened] = useToggleAlert(false);
-
-  function onSelectCurrency(code: string) {
-    const currency = currencies.filter(
-      (currency: any) => currency.code === code
-    );
-    setCurrencyA(currency[0]);
-    setCurrencyAval(currencyBval * currencyA.sellRate);
-  }
-
-  function onChangeHandler(e: ChangeEvent<HTMLInputElement>, currency: string) {
-    if (currency === "A") {
-      const newValueA: any | number = e.target.value; // Should be just number, dunno why TypeScript complains.
-      setCurrencyAval(newValueA);
-      setCurrencyBval(newValueA / currencyA.sellRate);
-    } else if (currency === "B") {
-      const newValueB: any | number = e.target.value; // Should be just number, dunno why TypeScript complains.
-      setCurrencyAval(newValueB * currencyA.sellRate);
-      setCurrencyBval(newValueB);
-    }
-  }
 
   return (
     <div className={styles.container}>
