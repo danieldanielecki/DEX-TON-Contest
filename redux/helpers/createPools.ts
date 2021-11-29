48a08214ca90fa5feafa2f870d4c608393bfcbe7
Daniel Danielecki <daniel.danielecki@mail.com>
2021-11-29 12:06:25 +0100

    Add: negative values for % 24h change

--- redux/helpers/createPools.ts
+++ redux/helpers/createPools.ts
@@ -5,11 +5,17 @@ export default async function createPools() {
   const namesArray = currencies.map((currency: { symbol: string }) =>
     currency.symbol.toUpperCase()
   );
+
+  // Generate random numbers for the pool statistics page.
   const allCombinations = namesArray.flatMap((symbol: string, index: number) =>
     namesArray.slice(index + 1).map((nextSymbol: string) => ({
       marketCap: Math.floor(Math.random() * (1000 - 1) + 1),
       pair: `${symbol}/${nextSymbol}`,
-      priceChangePercentage24h: (Math.random() * (20 - 1) + 1).toFixed(2),
+      priceChangePercentage24h: (
+        Math.random() *
+        29 *
+        (Math.round(Math.random()) ? 1 : -1)
+      ).toFixed(2),
       volume: Math.floor(Math.random() * (100 - 1) + 1),
     }))
   );
