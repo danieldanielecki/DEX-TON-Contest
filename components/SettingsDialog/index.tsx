e33a1ab475c580d53fbb0e3cbf7156adbb69cf32
Daniel Danielecki <daniel.danielecki@mail.com>
2021-11-29 20:27:21 +0100

    Backup: working tooltip for 1 switch

--- components/SettingsDialog/index.tsx
+++ components/SettingsDialog/index.tsx
@@ -65,13 +65,54 @@ const SettingsDialog = (props: {
                 </div>
               </div>
               <div className={styles.toggles}>
-                <div>
+                <div className="d-flex align-items-center justify-content-between">
                   <ToggleOnOffSwitch
                     checked={autoRouter}
                     id="auto_router"
                     onChange={onAutoRouterChange}
                     title="Auto Router"
                   />
+                  <span className={styles.tooltip}>
+                    <span className={styles.tooltip_text}>?</span>
+                  </span>
+                  <svg
+                    className={styles.popup_bg}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 300 130"
+                    height="130"
+                    width="300"
+                  >
+                    <path
+                      d="M36.5 12.695c15.9-2.4 32.556-4.284 82.977-3.815 79.67.74 121.785.26 145.294 5.51 18.483 4.13 34.333 11.696 33.382 32.11l-1.696 36.39c-1.01 21.68-11.678 29.377-21.934 30.838-14.884 2.12-29.72 3.52-54.512-.848C232.522 118.263 233.5 129 233.5 129s-1.992-7.686-32.218-14c-17.933-5.043-118.204 3.687-163.51-2.544-21.317-2.932-33.706-8.26-34.228-27.022L2.272 39.717c-.46-16.58 12.34-23.718 34.23-27.022z"
+                      fill="#303030"
+                      stroke="#000"
+                    />
+                  </svg>
+                  <svg
+                    className={styles.popup_outline}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 300 130"
+                    height="130"
+                    width="300"
+                  >
+                    <g stroke-width="2" stroke-linecap="round">
+                      <path
+                        className={styles.popup_outline_left}
+                        d="M233.5 129s-1.992-7.686-32.218-14c-17.933-5.043-118.204 3.687-163.51-2.544-21.317-2.932-33.706-8.26-34.228-27.022L2.272 39.717c-.46-16.58 12.34-23.718 34.23-27.022 15.897-2.4 32.554-4.284 82.975-3.815"
+                        fill="none"
+                        stroke="#303030"
+                      />
+                      <path
+                        className={styles.popup_outline_right}
+                        d="M119.477 8.88c79.67.74 121.785.26 145.294 5.51 18.483 4.13 34.333 11.696 33.382 32.11l-1.696 36.39c-1.01 21.68-11.678 29.377-21.934 30.838-14.884 2.12-29.72 3.52-54.512-.848C232.522 118.263 233.5 129 233.5 129"
+                        fill="none"
+                        stroke="#303030"
+                      />
+                    </g>
+                  </svg>
+                  <div className={styles.popup_text}>
+                    Sample explaining text goes here.
+                  </div>
                 </div>
                 <div>
                   <ToggleOnOffSwitch
