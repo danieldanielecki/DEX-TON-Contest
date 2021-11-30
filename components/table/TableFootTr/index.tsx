5403f084966d1f0f47dae5c6522c5562d89549fe
Katarzyna Janiszewska <katarzyna.janiszewska@gmail.com>
2021-11-30 14:13:29 +0100

    table files organizationa and general styling

---
+++ components/table/TableFootTr/index.tsx
@@ -0,0 +1,50 @@
+import "regenerator-runtime/runtime"; // Fixes "ReferenceError: regeneratorRuntime is not defined".
+import React from "react";
+import BaseButton from "../../BaseButton";
+
+const TableFootTr = (props: { tableSettings: any }) => {
+  const { tableSettings } = props;
+  return (
+    <tr className="text-center">
+      <td colSpan={5}>
+        <BaseButton
+          disabled={!tableSettings.canPreviousPage}
+          onClick={() => tableSettings.gotoPage(0)}
+          title="First"
+        />
+        <BaseButton
+          disabled={!tableSettings.canPreviousPage}
+          onClick={() => tableSettings.previousPage()}
+          title="Previous"
+        />
+        <p
+          style={{
+            display: "inline-block",
+            margin: 20,
+            verticalAlign: "bottom",
+          }}
+        >
+          Page{" "}
+          <strong>
+            {`${tableSettings.state.pageIndex + 1} of ${tableSettings.pageOptions.length
+              }`}
+          </strong>{" "}
+        </p>
+        <BaseButton
+          disabled={!tableSettings.canNextPage}
+          onClick={() => tableSettings.nextPage()}
+          title="Next"
+        />
+        <BaseButton
+          disabled={!tableSettings.canNextPage}
+          onClick={() =>
+            tableSettings.gotoPage(tableSettings.pageCount - 1)
+          }
+          title="Last"
+        />
+      </td>
+    </tr>
+  );
+};
+
+export default TableFootTr;
