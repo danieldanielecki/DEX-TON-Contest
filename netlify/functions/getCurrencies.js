512b0d783622d96faa898ac0a22b9644d26ad9bc
Katarzyna Janiszewska <katarzyna.janiszewska@gmail.com>
2021-11-30 09:53:59 +0100

    attempt to make lambda working

--- netlify/functions/getCurrencies.js
+++ netlify/functions/getCurrencies.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 const coins = require('./stubs/coins/coins.json');
 
-const API_ENDPOINT = "/simple/price";
+const API_ENDPOINT = "simple/price";
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
