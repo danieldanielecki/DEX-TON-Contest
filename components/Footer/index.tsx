aca42e5169782f00fc0e042c6702cd64ae9a05a0
Daniel Danielecki <daniel.danielecki@mail.com>
2021-11-30 17:09:35 +0100

    Change: align footer styles to ton.org

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,19 +1,9 @@
-import Image from "next/image";
 import styles from "./styles.module.scss";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <span className={styles.logo}>
-        Powered by
-      </span>
-      <Image
-        alt="The Open Network (TON)"
-        height={50}
-        src="/logo_white.svg"
-        title="The Open Network (TON)"
-        width={120}
-      />
+      <span>Authors: Daniel Danielecki &amp; Katarzyna Janiszewska</span>
     </footer>
   );
 };
