684ee13b671a04dff913a083327f74eaa5be9745
Daniel Danielecki <daniel.danielecki@mail.com>
2021-12-23 20:13:52 +0100

    Fix: Vercel deployment

--- services/getCurrencies.js
+++ services/getCurrencies.js
@@ -1,7 +1,6 @@
-import coins from '../stubs/coins.json'
+import coins from "../stubs/coins.json";
 
+// Here we had a call (https://github.com/danieldanielecki/DEX-TON-Contest/blob/82e32171c967242cfa8a361b74c873c5b5b2ab7c/services/getCurrencies.js) to our standalone Express server (https://github.com/danieldanielecki/DEX-TON-Contest/blob/82e32171c967242cfa8a361b74c873c5b5b2ab7c/ssr-server.js), but due to deployment issue we've simplified it. It's a perfect place to fetch your API to get the data you want.
 export default function getCurrencies() {
-  return fetch('simple/price')
-    .then(res => res.json())
-    .catch(error => error.message)
+  return coins;
 }
